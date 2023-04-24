e1dc304d033e009fee3ad9760d81fbb92831001f
Fabian Heilig <fabianheilig@MacBook-Pro-de-Fabian.local>
2023-04-24 13:16:36 +0200

    add all questions

--- index.php
+++ index.php
@@ -9,37 +9,239 @@
 </head>
 <body>
 <h1>Diagnostique</h1>
-<h3>Nom de l'entreprise</h3>
+<h2>Nom de l'entreprise</h2>
+<!--Chaque axe à un btn pour afficher son contenu-->
 <button onclick="hideAndDisplay('contenu1')">Axe Compétences</button>
 <br>
+<!--On affiche le contenu de l'axe en question si on a cliqué sur le btn-->
 <div id="contenu1" style="display: none">
-    <h4>Excellence Technique/Communauté de pratiques</h4>
-    <p>Les questions</p>
-    <h4>Faire agile</h4>
-    <p>Les questions</p>
+    <h3>Excellence Technique/Communauté de pratiques</h3>
+    <!--On affiche les questions de la catégorie Le label, la réponse et un conseil-->
+    <div>
+        <h4>Formations pour Apprendre à apprendre, changement de paradigme, "être à la page" (au-delà des compétences "justes" nécessaires)</h4>
+        <div>La réponse</div>
+        <div>Le conseil</div>
+    </div>
+    <br>
+    <div>
+        <h4>Le co-développement (outil d'intelligence collective) existe-t-il dans l'entreprise ?</h4>
+        <div>La réponse</div>
+        <div>Le conseil</div>
+    </div>
+    <br>
+    <div>
+        <h4>Les collaborateurs sont-ils amenés à partager leurs compétences et sous quelles formes ?</h4>
+        <div>La réponse</div>
+        <div>Le conseil</div>
+    </div>
+    <br>
+    <div>
+        <h4>Le mentoring (fonctionnement en binôme) existe-t-il pour assurer le transfert de compétences ?</h4>
+        <div>La réponse</div>
+        <div>Le conseil</div>
+    </div>
+    <br>
+    <div>
+        <h4>Les managers sont-ils aussi formateurs sur certains sujet pour l'entreprise entière ?</h4>
+        <div>La réponse</div>
+        <div>Le conseil</div>
+    </div>
+    <br>
+    <div>
+        <h4>L'entreprise favorise-t-elle l'excellence technique ? (Principe 9 du Manifeste Agile)</h4>
+        <div>La réponse</div>
+        <div>Le conseil</div>
+    </div>
+    <h3>Faire agile</h3>
+    <div>
+        <h4>Déployez vous les pratiques du "Faire Agile", c'est-à-dire une ou plusieurs des "méthodes" agiles ?</h4>
+        <div>La réponse</div>
+        <div>Le conseil</div>
+    </div>
+    <br>
+    <div>
+        <h4>Votre entreprise promeut-elle un "état d'esprit agile" ?</h4>
+        <div>La réponse</div>
+        <div>Le conseil</div>
+    </div>
+    <h3>Gestion humaine des compétences</h3>
+    <div>
+        <h4>Votre entreprise gère-t-elle humainement les compétences ?</h4>
+        <div>La réponse</div>
+        <div>Le conseil</div>
+    </div>
 </div>
 <br>
 <button onclick="hideAndDisplay('contenu2')">Axe Réactivité</button>
 <br>
 <div id="contenu2" style="display: none">
-    <h4>Vélocité de réponse</h4>
-    <p>Les questions</p>
-    <h4>Environnements souples</h4>
-    <p>Les questions</p>
-    <h4>Défi environnemental</h4>
-    <p>Les questions</p>
-    <h4>Veille et benchmark</h4>
+    <h3>Vélocité de réponse</h3>
+    <div>
+        <h4>Valeur supérieure utilisable livrée plus tôt (Fonction principale utilisable dès les premières versions)</h4>
+        <div>La réponse</div>
+        <div>Le conseil</div>
+    </div>
+    <div>
+        <h4>Réactivité face aux impératifs prépondérants</h4>
+        <div>La réponse</div>
+        <div>Le conseil</div>
+    </div>
+    <div>
+        <h4>Mesure de la vélocité de l'équipe (indicateur de suivi du travail d'une équipe de conception)</h4>
+        <div>La réponse</div>
+        <div>Le conseil</div>
+    </div>
+    <h3>Environnements souples</h3>
+    <div>
+        <h4>Les installations techniques et de gestion sont modernes (TIC/ERP/CRM)</h4>
+        <div>La réponse</div>
+        <div>Le conseil</div>
+    </div>
+    <div>
+        <h4>Les équipes sont en capacité d'autonomiser une partie de leurs tâches</h4>
+        <div>La réponse</div>
+        <div>Le conseil</div>
+    </div>
+    <div>
+        <h4>Les équipes s’inscrivent dans un cadre Agile Lean</h4>
+        <div>La réponse</div>
+        <div>Le conseil</div>
+    </div>
+    <div>
+        <h4>Les mécanismes de livraison et de synchronisation sont matures</h4>
+        <div>La réponse</div>
+        <div>Le conseil</div>
+    </div>
+    <h3>Défi environnemental</h3>
+    <div>
+        <h4>Les innovations produit tiennent compte de l'urgence climatique</h4>
+        <div>La réponse</div>
+        <div>Le conseil</div>
+    </div>
+    <div>
+        <h4>Les processus internes sont remis en cause pour diminuer leur impact environnemental</h4>
+        <div>La réponse</div>
+        <div>Le conseil</div>
+    </div>
+    <div>
+        <h4>Les parties prenantes sont sélectionnées en fonction de leur éthique vis-à-vis du développement durable</h4>
+        <div>La réponse</div>
+        <div>Le conseil</div>
+    </div>
+    <h3>Veille et benchmark</h3>
+    <div>
+        <h4>Veille stratégique</h4>
+        <div>La réponse</div>
+        <div>Le conseil</div>
+    </div>
 </div>
 <br>
 <button onclick="hideAndDisplay('contenu3')">Axe Numérique</button>
 <br>
 <div id="contenu3" style="display: none">
-    <h4>Business model</h4>
-    <p>Les questions</p>
-    <h4>Relation client</h4>
-    <p>Les questions</p>
-    <h4>Management</h4>
-    <p>Les questions</p>
+    <h3>Business model</h3>
+    <div>
+        <h4>Votre entreprise dégage t-elle une part de CA  par des produits ou services en ligne</h4>
+        <div>La réponse</div>
+        <div>Le conseil</div>
+    </div>
+    <div>
+        <h4>La mise en place d’outils numériques a-t-elle permis d'optimiser les coûts, les délais et la qualité ?</h4>
+        <div>La réponse</div>
+        <div>Le conseil</div>
+    </div>
+    <div>
+        <h4>Comment les outils sont-ils intégrés dans les process de l’entreprise ?</h4>
+        <div>La réponse</div>
+        <div>Le conseil</div>
+    </div>
+    <div>
+        <h4>Comment partagez-vous les données numériques avec les parties prenantes (clients, fournisseurs,…) ?</h4>
+        <div>La réponse</div>
+        <div>Le conseil</div>
+    </div>
+    <div>
+        <h4>Mesurez-vous les impacts du numérique sur votre entreprise ?</h4>
+        <div>La réponse</div>
+        <div>Le conseil</div>
+    </div>
+    <div>
+        <h4>Quel est l’impact du numérique sur la démarche RSE de l’entreprise ?</h4>
+        <div>La réponse</div>
+        <div>Le conseil</div>
+    </div>
+    <div>
+        <h4>Existe-t-il des freins (stratégiques, financiers,…) à l’ investissement dans les outils numériques ?</h4>
+        <div>La réponse</div>
+        <div>Le conseil</div>
+    </div>
+    <h3>Relation client</h3>
+    <div>
+        <h4>L’entreprise dispose-t-elle : d’un site internet, d’un compte LinkedIn, d’une page Facebook, d’un compte Twitter,... ?</h4>
+        <div>La réponse</div>
+        <div>Le conseil</div>
+    </div>
+    <div>
+        <h4>Comment utilisez-vous le numérique dans la relation client ? (nouveau modèle de vente, nouveau modèle d’échanges avec les clients, communauté, story, chat,...)</h4>
+        <div>La réponse</div>
+        <div>Le conseil</div>
+    </div>
+    <div>
+        <h4>L’entreprise dispose-t-elle d’un community manager ?</h4>
+        <div>La réponse</div>
+        <div>Le conseil</div>
+    </div>
+    <div>
+        <h4>Certains de vos collaborateurs sont-ils actifs sur les réseaux sociaux au nom de l’entreprise ?</h4>
+        <div>La réponse</div>
+        <div>Le conseil</div>
+    </div>
+    <div>
+        <h4>Comment mesurez-vous et exploitez-vous les données issues du site de votre entreprise ?</h4>
+        <div>La réponse</div>
+        <div>Le conseil</div>
+    </div>
+    <div>
+        <h4>Avez-vous observé chez vos concurrents des pratiques innovantes ?</h4>
+        <div>La réponse</div>
+        <div>Le conseil</div>
+    </div>
+    <h3>Management</h3>
+    <div>
+        <h4>Vos collaborateurs sont-ils équipés de nouveaux équipements numériques de travail (PC portable, tablette, smartphone,…) ? </h4>
+        <div>La réponse</div>
+        <div>Le conseil</div>
+    </div>
+    <div>
+        <h4>L’arrivée des outils digitaux a-t-elle eu un impact sur les pratiques et la culture de l’entreprise ?</h4>
+        <div>La réponse</div>
+        <div>Le conseil</div>
+    </div>
+    <div>
+        <h4>Comment vous êtes-vous approprié et comment avez-vous accompagné ces évolutions?</h4>
+        <div>La réponse</div>
+        <div>Le conseil</div>
+    </div>
+    <div>
+        <h4>Les nouvelles possibilités technologiques ont-elles fait évoluer le modèle d’organisation de l’entreprise et/ou votre management, vers plus de collaboration avec des acteurs internes ou externes ?</h4>
+        <div>La réponse</div>
+        <div>Le conseil</div>
+    </div>
+    <div>
+        <h4>Mobilisez-vous des outils de veille et mettez-vous en œuvre des modalités de curation et de partage de l’ information ?</h4>
+        <div>La réponse</div>
+        <div>Le conseil</div>
+    </div>
+    <div>
+        <h4>Les fonctionnalités des outils sont-elles augmentées par la pratique de vos collaborateurs ?</h4>
+        <div>La réponse</div>
+        <div>Le conseil</div>
+    </div>
+    <div>
+        <h4>Comment accompagnez-vous vos collaborateurs dans la transition numérique ?</h4>
+        <div>La réponse</div>
+        <div>Le conseil</div>
+    </div>
 </div>
 <br>
 </body>
