e104df58f7b3c3493fe415823786351652d6a983
Fabian Heilig <fabianheilig@MacBook-Pro-de-Fabian.local>
2023-04-26 11:25:23 +0200

    add error display on grid_list

--- grid_list.php
+++ grid_list.php
@@ -1,8 +1,8 @@
 <?php
 //On affiche les erreurs
-/*ini_set('display_errors', 1);
+ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);*/
+error_reporting(E_ALL);
 require_once ('src/Entity/DatabaseConnection.php');
 require_once ('src/Entity/Grids.php');
 $objGrid = new Grids();
